14fae372381f5c13f4a6f9f2e2568580c5bdab2c
Terence Berry <berryte@flip2.engr.oregonstate.edu>
2019-02-10 15:48:37 -0800

    Basic layout

--- projects/berryte/dominion/randomtestadventurer.c
+++ projects/berryte/dominion/randomtestadventurer.c
@@ -18,7 +18,7 @@
 
 //Return random number within an upper and lower bound
 int randomNum(int min, int max){ 
-	int rNum; //random number to return;
+	int rNum = 0; //random number to return;
 	/* rand() returns between 0 and max - 1*/
 	rNum = (rand() % (max - min + 1)) + min; // get the range (max-min) add 1 and lower bound
 	return rNum;
@@ -32,19 +32,141 @@ int main(){
 	while(numTests != 200){
 		printf("\nTest %d:\n", numTests + 1);
 		int temp[MAX_HAND]; //should be refactored out as a parameter
-		int numPlayers = randomNum(2, 4); //can only accept 2-4 players
+		int numPlayers = randomNum(2, MAX_PLAYERS); //can only accept 2-4 players
 		printf("\tNumber of Players: %d\n", numPlayers);
 		//start game
 		struct gameState testGame;
 		int game = initializeGame(numPlayers, cards, 1000, &testGame);
-		int cardSize = 10; //we initially start with 10 cards
-		int numHand = rand() % cardSize; //number of cards in hand (0 to cardSize)
-		int numCoppers = rand() % cardSize; //num of coppers (0 to cardSize)
-		int numSilver;// = rand() % 10;
-		int numGold;// = rand() % 10;
-		int numDeck; //number in deck
-		int numDiscard; //number of cards in discard
 		
+		//Random Card Numbers and how many in each pile
+		int cardSize = 10; //we initially start with 10 cards 
+		int numHand = randomNum(1, cardSize); //number of cards in hand (1 to cardSize)
+		int numDeck = randomNum(0, cardSize - numHand) ; //num of cards in deck
+		int numDiscard = cardSize - numHand - numDeck; //the rest	
+
+		//Random Treasure
+		int numTreasure = randomNum(0, cardSize); // 0 to cardsize
+		int numCoppers = randomNum(0, numTreasure); //num of coppers (0 to cardSize)
+		int numSilver =  (randomNum(0, numTreasure - numCoppers)); //num silver
+		int numGold = numTreasure - numCoppers - numSilver; // rest
+		int tHand = 0; //treasure in hand
+		int tDeck = 0; //treasure in deck
+		int tDiscard = 0; //treasure in discard
+
+		//Set up deck with random information for all players  
+		int i;
+		for(i = 0; i < numPlayers; i++){
+			//Alter hand
+			int j;
+			int co = 0; //copper
+			int si = 0; //silver
+			int go = 0; //gold
+
+			testGame.handCount[i] = numHand;
+			testGame.hand[i][0] = adventurer;
+			for(j = 1; j < testGame.handCount[i]; j++){
+				testGame.hand[i][j] = province; //fill in the card
+				if(co != numCoppers){
+					testGame.hand[i][j] = copper;
+					co++;
+					tHand++;
+				}
+				else if(si != numSilver){
+					testGame.hand[i][j] = silver;
+					si++;
+					tHand++;
+				}
+				else if(go != numGold){
+					testGame.hand[i][j] = gold;
+					go++;
+					tHand++;
+				}	
+			}
+
+			//Alter Deck
+			testGame.deckCount[i] = numDeck;
+			for(j = 0; j < testGame.deckCount[i]; j++){
+				testGame.hand[i][j] = province; //fill in the card
+                                if(co != numCoppers){
+                                        testGame.hand[i][j] = copper;
+                                        co++;
+					tDeck++;
+                                }
+                                else if(si != numSilver){
+                                        testGame.hand[i][j] = silver;
+                                        si++;
+					tDeck++;
+                                }
+                                else if(go != numGold){
+                                        testGame.hand[i][j] = gold;
+                                        go++;
+					tDeck++;
+                                }
+			}
+
+			//Alter Discard
+			testGame.discardCount[i] = numDiscard;
+			for(j = 0; j < testGame.discardCount[i]; j++){
+				testGame.hand[i][j] = province; //fill in the card
+                                if(co != numCoppers){
+                                        testGame.hand[i][j] = copper;
+                                        co++;
+					tDiscard++;
+                                }
+                                else if(si != numSilver){
+                                        testGame.hand[i][j] = silver;
+                                        si++;
+					tDiscard++;
+                                }
+                                else if(go != numGold){
+                                        testGame.hand[i][j] = gold;
+                                        go++;
+					tDiscard++;
+                                }
+			}
+			//PlayAdventurer for this player
+/*			playAdventurer(&testGame, i, temp);
+
+			//Test if implemented correctly
+
+			//If there were 2 treasure cards not in the hand
+			//they should have been added to the hand
+			if((tDiscard + tDeck) >= 2){
+				//Iterate through hand and there should be 2 more treasure cards
+				int k;
+				int handTreasures = 0;
+				printf("\tPlayer %d test: 2 Treasure cards added to hand\n", i + 1);
+				for(k = 0; k < testGame.handCount[i]; k++){
+					if(testGame.hand[i][k] == copper || testGame.hand[i][k] == silver || testGame.hand[i][k] == gold){
+						handTreasures++;
+					}
+				}
+				if(handTreasures == (tHand + 2)){
+					printf("\t\tTest Successful!\n");
+				}
+				else{
+					printf("\t\tTest Failed. 2 Treasure cards were not added to the hand.\n");	
+				}
+			}
+			else if((tDiscard + tDeck) == 1){
+				//Iterate through hand and there should be 2 more treasure cards
+				int k;
+				int handTreasures = 0;
+				printf("\tPlayer %d test: Only 1 Treasure card outside hand\n", i + 1);
+				for(k = 0; k < testGame.handCount[i]; k++){
+					if(testGame.hand[i][k] == copper || testGame.hand[i][k] == silver || testGame.hand[i][k] == gold){
+						handTreasures++;
+					}
+				}
+				if(handTreasures == (tHand + 2)){
+					printf("\t\tTest Successful!\n");
+				}
+				else{
+					printf("\t\tTest Failed. 2 Treasure cards were not added to the hand.\n");
+				}
+			}
+*/		}		
+
 		numTests++; //used to stop the tests
 	}
 	return 0;
