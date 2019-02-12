fdd0bf1596b23a5efd6d7e8b6d589cdae39bfbef
Terence Berry <berryte@flip3.engr.oregonstate.edu>
2019-02-11 16:20:29 -0800

    Updated files.

--- projects/berryte/dominion/randomtestcard1.c
+++ projects/berryte/dominion/randomtestcard1.c
@@ -12,10 +12,107 @@
 #include "rngs.h"
 #include <string.h>
 #include <stdio.h>
-#include <assert.h>
 #include <stdlib.h>
+#include <time.h>
+
+//Return random number within an upper and lower bound
+int randomNum(int min, int max){ 
+	int rNum = 0; //random number to return;
+	/* rand() returns between 0 and max - 1*/
+	rNum = (rand() % (max - min + 1)) + min; // get the range (max-min) add 1 and lower bound
+	return rNum;
+}
 
 int main(){
+	srand(time(NULL)); //to seed random num generator
+	int numTests = 0;
+	int cards[10] = {adventurer, smithy, council_room, mine, village, embargo, cutpurse, steward, remodel, feast};
+	printf("\n------------ Testing Random Smithy Card Function -------------\n");
+	while(numTests != 200){
+		printf("\nTest %d:\n", numTests + 1);
+		int numPlayers = randomNum(2, MAX_PLAYERS); //can only accept 2-4 players
+		//start game
+		struct gameState testGame;
+		initializeGame(numPlayers, cards, 1000, &testGame);
 
+		//Random Card Numbers and how many in each pile
+		int cardSize = 10; //we initially start with 10 cards 
+		int numHand = randomNum(1, cardSize); //number of cards in hand (1 to cardSize)
+		int numDeck = randomNum(0, cardSize - numHand) ; //num of cards in deck
+		int numDiscard = cardSize - numHand - numDeck; //the rest	
+	
+		//Set up cards
+		int i;
+		for(i = 0; i < numPlayers; i++){
+			int j;			
+			//Reset cards 
+			for(j = 0; j <= cardSize; j++){
+				testGame.hand[i][j] = province; //fill in the card
+				testGame.deck[i][j] = province;
+				testGame.discard[i][j] = province;
+			}
+			testGame.handCount[i] = numHand;
+			testGame.deckCount[i] = numDeck;
+			testGame.discardCount[i] = numDiscard;
+			// add smithy to hand of first player
+			testGame.hand[i][0] = smithy;
+		}
+
+		//Play Smithy
+		playSmithy(&testGame, rand() % testGame.handCount[0], 0); //randomly generate hand position
+		
+		//Run Tests
+		printf("\tTest: 3 cards removed from Deck\n");
+		if(numDeck >= 3){
+			if(numDeck - 3 == testGame.deckCount[0]){
+				printf("\t\tTest Successful!\n");
+			}
+			else{
+				printf("\t\tTest Failed. %d removed from deck.\n", numDeck - testGame.deckCount[0]);
+			}
+		}
+		else{//account for shuffle of discard pile into deck
+			int diff = numDeck - 3;
+			int newDeck;
+			if(numDiscard >= 3 && numDeck < 3){
+				newDeck = numDiscard + diff;
+			}
+			else if(numDiscard == 2 && numDeck == 2){
+				 newDeck = numDiscard + diff;
+			}
+			else if(numDeck + numDiscard <= 3){
+				newDeck = 0;
+			}
+
+			if(newDeck == testGame.deckCount[0]){
+				printf("\t\tTest Successful!\n");
+			}
+			else{
+				printf("\t\tTest Failed. After shuffle, deck count is %d.\n", testGame.deckCount[0]);
+			}
+		}
+
+		printf("\tTest: Hand Count Increased by 2. (Draw 3 - Discard Smithy)\n");
+		if(numDeck + numDiscard >= 3){
+			if(numHand + 2 == testGame.handCount[0]){
+				printf("\t\tTest Successful!\n");
+			}
+			else{
+				printf("\t\tTest Failed. %d added to hand.\n", testGame.handCount[0] - numHand);
+			}
+		}
+		else{ //less than 3 cards available
+			if(testGame.handCount[0] - numHand == numDeck + numDiscard){
+				printf("\t\tTest Successful!\n");
+			}
+			else{
+				printf("\t\tTest Failed. %d added to hand.\n", testGame.handCount[0] - numHand);		
+			}
+		}
+		
+
+
+		numTests++;
+	}
 	return 0;
 }
--- projects/berryte/dominion/randomtestcard2.c
+++ projects/berryte/dominion/randomtestcard2.c
@@ -15,7 +15,62 @@
 #include <assert.h>
 #include <stdlib.h>
 
-int main(){
 
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include "rngs.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+//Return random number within an upper and lower bound
+int randomNum(int min, int max){ 
+	int rNum = 0; //random number to return;
+	/* rand() returns between 0 and max - 1*/
+	rNum = (rand() % (max - min + 1)) + min; // get the range (max-min) add 1 and lower bound
+	return rNum;
+}
+
+int main(){
+	srand(time(NULL)); //to seed random num generator
+	int numTests = 0;
+	int cards[10] = {adventurer, smithy, council_room, mine, village, embargo, cutpurse, steward, remodel, feast};
+	printf("\n------------ Testing Random Adventurer Card Function -------------\n");
+	while(numTests != 200){
+		printf("\nTest %d:\n", numTests + 1);
+		int numPlayers = randomNum(2, MAX_PLAYERS); //can only accept 2-4 players
+		//start game
+		struct gameState testGame;
+		initializeGame(numPlayers, cards, 1000, &testGame);
+		
+		//Random Card Numbers and how many in each pile
+		int cardSize = 10; //we initially start with 10 cards 
+		int numHand = randomNum(1, cardSize); //number of cards in hand (1 to cardSize)
+		int numDeck = randomNum(0, cardSize - numHand) ; //num of cards in deck
+		int numDiscard = cardSize - numHand - numDeck; //the rest	
+	
+		//Set up cards
+		int i;
+		for(i = 0; i < numPlayers; i++){
+			int j;			
+			//Reset cards 
+			for(j = 0; j <= cardSize; j++){
+				testGame.hand[i][j] = province; //fill in the card
+				testGame.deck[i][j] = province;
+				testGame.discard[i][j] = province;
+			}
+			testGame.handCount[i] = numHand;
+			testGame.deckCount[i] = numDeck;
+			testGame.discardCount[i] = numDiscard;
+			// add smithy to hand of first player
+			testGame.hand[i][0] = council_room;
+		}
+
+		//Play Council Room
+		playCRoom(&testGame, 0, rand() % testGame.handCount[0]); //randomly generate hand position
+
+		numTests++;
+	}
 	return 0;
 }
